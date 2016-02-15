e4235ba2a410bd4f1b4df26248057056ba793c51
Praveen <praveentheace@gmail.com>
2016-02-15 11:39:40 +0530

    take data out of view to controller

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
+var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
-var router = express.Router();
 
-/* Locations Page. */
+/* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
 
-/* Others Page. */
+/* Other pages */
 router.get('/about', ctrlOthers.about);
 
 module.exports = router;
